67b3f7fdb0133173c7f71a9d71d66947df5b3f73
raouftiliouine <abtili99@gmail.com>
2014-01-31 22:34:23 -0500

    Add Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tagrid::Application.config.secret_key_base = '37ac6cad676ebd0fcb6dad238d3911188ef2cd3311c337cac1f001f7f79f072deff31fea742710dc85b30e0ee433a645c21839d877ae3ff665d929e4450f5055'
