df675a86a29fad5c640684e11fb4d7c392fb3fe3
Raouf Tiliouine <abtili99@gmail.com>
2014-02-02 01:48:41 -0500

    Add Pins Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
          
   # Setup accessible (or potected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  
+  has_many :pins
 end
